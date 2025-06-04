967e064415dfbd9ac2abbad69204338ceb7644e3
Yoav Tzfati <yoav.tzfati@gmail.com>
2025-06-04 08:52:17 -0700

    simplify auth config further

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,10 +2,7 @@ const authConfig = {
   providers: [
     {
       // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-      domain:
-        "CLERK_JWT_ISSUER_DOMAIN" in process.env
-          ? process.env.CLERK_JWT_ISSUER_DOMAIN
-          : "https://workable-dog-93.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
